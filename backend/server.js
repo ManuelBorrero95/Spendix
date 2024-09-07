996cb21b3b1c3f4f9af874fb8d461c315f236cb3
Manuel Borrero <mborrero@onturtle.eu>
2024-09-07 11:03:04 +0200

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,9 @@ const corsOptions = {
     // Queste sono gli URL da cui il nostro frontend farà richieste al backend.
     const whitelist = [
       'http://localhost:5173', // Frontend in sviluppo
-      'https://mern-blog-part-v.vercel.app/', // Frontend in produzione (prendere da vercel!)
-      'https://mern-blog-ctt3.onrender.com' // URL del backend (prendere da render!)
+      'http://localhost:5174', // Frontend in sviluppo
+      'https://spendix.vercel.app/', // Frontend in produzione (prendere da vercel!)
+      'https://spendix.onrender.com' // URL del backend (prendere da render!)
     ];
     
     if (process.env.NODE_ENV === 'development') {
