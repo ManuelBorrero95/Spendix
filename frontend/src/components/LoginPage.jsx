aeea9983c76adf7dbbf09b6d75725db0ebb20d9e
Manuel Borrero <mborrero@onturtle.eu>
2024-09-07 13:08:55 +0200

    change loginpage

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate, Link } from 'react-router-dom';
 
-const BACKEND_URL = process.env.VITE_API_URL || 'http://localhost:5000';
+//const BACKEND_URL = process.env.VITE_API_URL || 'http://localhost:5000';
 
-//const BACKEND_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+const BACKEND_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 
 const LoginPage = () => {
   const [email, setEmail] = useState('');
